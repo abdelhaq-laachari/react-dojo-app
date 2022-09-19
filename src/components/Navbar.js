86bfde8d9bdf67ef42a7f27c1bbaa446d5b6a37b
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-09-19 15:08:37 +0100

    outputting Lists

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,38 @@
- 
+import { useState } from 'react';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website 1",
+      body: "Lorem ipsum ....",
+      author: "Abdelhaq",
+      id: "1",
+    },
+    {
+      title: "My new website 2",
+      body: "Lorem ipsum ....",
+      author: "Yassine",
+      id: "2",
+    },
+    {
+      title: "My new website 3",
+      body: "Lorem ipsum ....",
+      author: "Kamal",
+      id: "3",
+    },
+  ]);
+
+
+return (
+  <div className="home">
+    {blogs.map((blog) => (
+      <div className="blog-preview" key={blog.id}>
+        <h2> {blog.title} </h2>
+        <span>Written by: {blog.author} </span>
+      </div>
+    ))}
+  </div>
+);
+};
+
+export default Home;
